c330310e716d0e80a793c99582cfe680e741aef5
Vinícius Braga <viniciusamgx@gmail.com>
2021-03-17 10:26:27 -0300

    JS Primeiros Passos

---
+++ 8-loops.js
@@ -0,0 +1,44 @@
+console.log(`\n Trabalhando com condicionais`);
+const listaDeDestinos = new Array(
+    `Salvador`,
+    `São Paulo`,
+    `Rio de Janeiro`,
+    `Curitiba`
+    );
+
+const idadeComprador = 18;
+const estaAcompanhada = false;
+let temPassagemComprada = false;
+const destino = "São Paulo"
+const podeComprar = idadeComprador >=18 || estaAcompanhada == true;
+
+console.log("\n Destinos possíveis:");
+console.log(listaDeDestinos);
+
+let contador =0;
+let destinoExiste = false;
+while(contador<3){
+    if(listaDeDestinos[contador] == destino){
+        console.log("Destino Existe")
+        destinoExiste = true;
+        break;
+    }
+    contador += 1;
+}
+
+console.log("Destino existe: ", destinoExiste);
+
+if(podeComprar && destinoExiste){
+    console.log("Boa Viagem");
+}else{
+    console.log("Desculpe tivemos um erro!");
+}
+
+for(let i = 0; i < 3 ; i++){
+    if(listaDeDestinos[contador] == destino){
+        console.log("Destino Existe")
+        destinoExiste = true;
+        break;
+    }
+
+}
